c59f40f7d9b85597189d427bf42c828ff09e9774
Rudhram Saraswat <rudhramsaraswat2770@gmail.com>
2024-05-14 15:16:30 +0530

    chore: Add HttpClientModule to ApiService spec

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ApiService', () => {
   let service: ApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ]
+    });
     service = TestBed.inject(ApiService);
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have a method getUser', () => {
+    expect(service.getUser).toBeTruthy();
+  });
+
+  it('should have a method getSearchTerm', () => {
+    expect(service.getSearchTerm).toBeTruthy();
+  });
+
+  it('should have a method setTotalPages', () => {
+    expect(service.setTotalPages).toBeTruthy();
+  });
+
+  it('should have a method setCurrPage', () => {
+    expect(service.setCurrPage).toBeTruthy();
+  });
+
+  it('should have a method getRepos', () => {
+    expect(service.getRepos).toBeTruthy();
+  });
+
+
 });
